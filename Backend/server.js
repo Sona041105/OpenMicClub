ed1d9a2a25f93aed226418fcdb15ec892cc39ae1
Muskan Mishra <muskanbhargav05@gmail.com>
2022-06-26 17:43:53 +0530

    heroku-4

--- Backend/server.js
+++ Backend/server.js
@@ -36,11 +36,14 @@ app.use(router);
 const path = require("path");
 
 // Step 1:
+if(process.env.NODE_ENV=="production"){
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 });
+
+}
 
 app.get('/', (req, res) => {
     res.send('Hello from express Js');
